ad713c342ca57e49fb86fb74990a040e53c1daf4
sahil-sahu <sahilku2003@gmail.com>
2023-10-28 01:07:32 +0530

    added queries to fetch inventory items

--- src/resolvers/resolvers.ts
+++ src/resolvers/resolvers.ts
@@ -2,11 +2,13 @@ import banner from "../adminResolvers/banner";
 import auth from "./auth";
 import customer from "./customer";
 import inventory from "./inventory";
+import ad_inventory from "../adminResolvers/inventory";
 
 const resolvers = {
   Query: {
     verifyOtp : auth.verifyOtp,
     getAddresses : customer.getAddresses,
+    getInventoryItem : ad_inventory.getItem,
     banner: banner.carouselView,
     inventoryItems: inventory.getItems,
   },
